1d706245700b5edc2f19674c12d6cdcb696c5450
tjhax <59748258+tjhax@users.noreply.github.com>
2020-05-11 20:52:24 -0700

    added level change and interaction with enemy
    Bugs to work on: jumping, npc going invisible after walking backwards, adding kill/respawn next time.

---
+++ 2d/Assets/Scenes/enemymove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class enemymove : MonoBehaviour { 
+
+public float speed;
+private bool MoveRight = true;
+
+public Transform groundDetection;
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        if(groundInfo.collider == false)
+        {
+            if(MoveRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                MoveRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                MoveRight = true;
+            }
+        }
+    }
+
+}
